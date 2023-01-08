b8eb66b532238a763da997d33945719a064972d5
Olivier Lefebvre <aguacongas@gmail.com>
2023-01-08 12:07:44 +0100

    docs: improve xml doc

--- src/Aguacongas.AspNetCore.Components.Maui.Authentication.Oidc/Abstraction/IAuthenticationStore.cs
+++ src/Aguacongas.AspNetCore.Components.Maui.Authentication.Oidc/Abstraction/IAuthenticationStore.cs
@@ -2,9 +2,29 @@
 
 namespace Aguacongas.AspNetCore.Components.Maui.Authentication.Oidc.Abstraction;
 
+/// <summary>
+/// Authentication store interface
+/// </summary>
 public interface IAuthenticationStore
 {
+    /// <summary>
+    /// Deletes a scope
+    /// </summary>
+    /// <param name="scope">The scope</param>
     void Delete(string scope);
+
+    /// <summary>
+    /// Gets <see cref="AuthenticationEntity"/> for a scope
+    /// </summary>
+    /// <param name="scope"></param>
+    /// <returns>The stored entity</returns>
     Task<AuthenticationEntity> GetAsync(string scope);
+
+    /// <summary>
+    /// Sets <see cref="AuthenticationEntity"/> for a scope
+    /// </summary>
+    /// <param name="scope">The scope</param>
+    /// <param name="value">The entity to store</param>
+    /// <returns></returns>
     Task SetAsync(string scope, AuthenticationEntity value);
 }
--- src/Aguacongas.AspNetCore.Components.Maui.Authentication.Oidc/Platforms/Windows/WebAuthenticator.cs
+++ src/Aguacongas.AspNetCore.Components.Maui.Authentication.Oidc/Platforms/Windows/WebAuthenticator.cs
@@ -24,6 +24,9 @@ namespace WinUIEx;
 /// </remarks>
 internal sealed class WebAuthenticator: IWebAuthenticator
 {
+    /// <summary>
+    /// Singleton instance of <see cref="WebAuthenticator"/>
+    /// </summary>
     public static readonly WebAuthenticator Instance = new();
 
     private readonly Dictionary<string, TaskCompletionSource<Uri>> tasks = new();
@@ -37,6 +40,11 @@ internal sealed class WebAuthenticator: IWebAuthenticator
         SubcribeToActivated(_appInstance);
     }
 
+    /// <summary>
+    /// Anthenticates the user
+    /// </summary>
+    /// <param name="webAuthenticatorOptions">The authentication options</param>
+    /// <returns></returns>
     public Task<WebAuthenticatorResult> AuthenticateAsync(WebAuthenticatorOptions webAuthenticatorOptions)
     => AuthenticateAsync(webAuthenticatorOptions.Url, webAuthenticatorOptions.CallbackUrl);
 
@@ -54,6 +62,10 @@ internal sealed class WebAuthenticator: IWebAuthenticator
             Trace.WriteLine($"WinUIEx: Failed to initialize the WebAuthenticator: {ex.Message}", "WinUIEx");
         }
     }
+
+    /// <summary>
+    /// Method call on application initialization.
+    /// </summary>
     public void OnAppCreation()
     {
         var activatedEventArgs = _appInstance?.GetActivatedEventArgs();
